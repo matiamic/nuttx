d78a398d0202c369a8c949490e2717e30b83e5f2
michal matias <mich4l.matias@gmail.com>
2025-06-29 15:32:51 +0200

    style

--- drivers/net/ncv7410.c
+++ drivers/net/ncv7410.c
@@ -163,7 +163,7 @@ static void ncv_release_rx_packet(FAR struct ncv7410_driver_s *priv);
 static inline void ncv_select_spi(FAR struct ncv7410_driver_s *priv);
 static inline void ncv_deselect_spi(FAR struct ncv7410_driver_s *priv);
 
-/* ncv7410 reset and configuration */
+/* NCV7410 reset and configuration */
 
 static int ncv_reset(FAR struct ncv7410_driver_s *priv);
 static int ncv_config(FAR struct ncv7410_driver_s *priv);
@@ -1611,7 +1611,7 @@ int ncv7410_initialize(FAR struct spi_dev_s *spi, int irq,
   priv->irqnum = irq;    /* Save the Interrupt Request Number       */
   priv->config = config; /* Save the reference to the configuration */
 
-  /* Reset NCV7410 chip */
+  /* Reset the NCV7410 chip */
 
   if (ncv_reset(priv))
     {
